d6ecac8f1efd1e1823a4b963505447a409fa0ecd
Tanay Salpekar <tsalpekar21@gmail.com>
2016-02-20 10:22:38 -0500

    regenerating auth token on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,11 @@ class SessionsController < ApplicationController
   before_action :authenticate, only: :destroy
 
   def create
-    u = User.find_by(token: params[:token])
+    u = User.find_by(email: params[:email])
 
    if u && u.authenticate(params[:password])
-     render json: {name: u.name, email: u.email}
+     u.regenerate_auth_token
+     render json: u
     else
       render json: {error: "Login credentials are invalid"}, status: :unauthorized
    end
