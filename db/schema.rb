e77158a11eac657c102b3e77ba579086c0c38806
Phil Prescher <phil.prescher.i@yello.co>
2016-02-20 01:18:42 -0500

    Payment update again

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220044821) do
+ActiveRecord::Schema.define(version: 20160220061114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "payments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "dare_id"
+    t.decimal  "amount",     null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "payments", ["dare_id"], name: "index_payments_on_dare_id", using: :btree
+  add_index "payments", ["user_id"], name: "index_payments_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name",            null: false
